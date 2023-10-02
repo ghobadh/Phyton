93579fa4352150e5b9bd73e9a93e393b741e4c46
Gavin Hashemi <Gavin.Hashemi@GForceSoftware.ca>
2023-10-02 12:07:02 -0400

    CreateConnection.py

---
+++ Connection.py
@@ -0,0 +1,16 @@
+'''
+@author Gavin Hashemi
+'''
+
+import requests
+import json
+import os
+import configparser
+from requests_ntlm import HttpNtlmAuth
+from cryptography.fernet import Fernet
+
+
+
+def the_result(arg1):
+  try:
+    c = configparser.
